572c687465653783817553767705a143b8ca1a0e
mhsu-sfsu <mhsu@sfsu.edu>
2024-11-15 18:23:47 -0800

    Question 1 Extra Credit

--- Part1/jacobsthalMemoization_EC.cpp
+++ Part1/jacobsthalMemoization_EC.cpp
@@ -5,9 +5,25 @@
 using namespace std;
 
 //TODO: add your documentation here
-// TO DO: implement jacobsthal
-//      Hint: it should return a long long (integers)
-//            it should take a cache (map or vector) of long longs as well
+/* This version of the function takes on a map called memo in addition to the integer that
+specifies which value should be returned. By passing the map by reference, it is a lot quicker
+to access values that have been calculated already, which greatly speeds up the amount of time
+it takes to calculate larger values of n.*/
+long long jacobsthal(int n, map<int, long long> &memo){
+    if (memo.count(n) == 1){ // If the value for n exists in the map, we just use it
+        return memo.find(n)->second;
+    } else { // If it doesn't exist in the map, we do the calculation, add it to the map, and return the value
+        if (n == 0 || n == 1){ // Adding values for 0 and 1
+            long long num = n;
+            memo.insert({n, num});
+            return n;
+        } else { // Case where n > 1 and is not in the map
+            long long calc = jacobsthal(n-1, memo) + (2 * jacobsthal(n-2, memo));
+            memo.insert({n, calc}); // Inserting into the map before returning 
+            return jacobsthal(n-1, memo) + (2 * jacobsthal(n-2, memo));
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
     // Read n from command line argument
